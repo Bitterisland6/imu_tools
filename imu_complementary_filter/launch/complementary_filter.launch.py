9c691b4c00a3641493e73167a0f82733fe30338c
Aleksander Szymański <bitterisland6@gmail.com>
2024-09-24 16:53:52 +0200

    format launch.py file
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- imu_complementary_filter/launch/complementary_filter.launch.py
+++ imu_complementary_filter/launch/complementary_filter.launch.py
@@ -7,18 +7,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
-    config = os.path.join(
-        get_package_share_directory('imu_complementary_filter'),
-        'config',
-        'filter_config.yaml'
-        )
-        
+    config = os.path.join(get_package_share_directory('imu_complementary_filter'), 'config', 'filter_config.yaml')
+
     node = Node(
         package='imu_complementary_filter',
         executable='complementary_filter_node',
         name='complementary_filter_gain_node',
         output='screen',
-        parameters = [config]
+        parameters=[config],
     )
     ld.add_action(node)
     return ld
